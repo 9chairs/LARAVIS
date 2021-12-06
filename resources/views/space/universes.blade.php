08db0b161a0672e302a041c60a60f603802f1553
Lucien van Duijnhoven <lucienvanduijnhoven@gmail.com>
2021-12-06 11:10:54 +0100

    Typo (non system related)

--- resources/views/space/universes.blade.php
+++ resources/views/space/universes.blade.php
@@ -3,7 +3,7 @@
         <div class="">
         @foreach ($universes as $universe)
             <h4 class="text-xl hover:text-red-300"><a href="{{ url('space/universes', [$universe->name]) }}">{{ $universe->name }}</a></h4>
-            <p class="text-sm">The 3 biggest planets:</p>
+            <p class="text-sm">The 3 oldest planets:</p>
             <ul>
             @foreach ($universe->planets as $planet)
                 <li class="text-sm hover:text-green-300"><a href="{{ url('/space/planets', $planet->id) }}">- {{ $planet->name }}</a></li>
